0d017a0546274e12fb907bc8d57be7b4a3da83a1
Rubengg5 <rubengg@uma.es>
2023-01-04 22:04:23 +0100

    Update environment.prod.ts

--- AngularApp/IngWeb-B3/src/environments/environment.prod.ts
+++ AngularApp/IngWeb-B3/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: "https://ingwebb3-back.herokuapp.com",
+  clientId: "182737891985-bhlpao31dj89qgu93i4pp3die544nomb.apps.googleusercontent.com",
+  cloudinaryURL: "https://res.cloudinary.com/dee6pfpam/image/upload/v1669835212/",
+  cloudinaryCloudName: "dee6pfpam",
+  cloudinaryApiUrl : "https://api.cloudinary.com/v1_1/dee6pfpam/upload",
+  cloudinaryUploadPreset : "ontg4fqa",
+  cloudinaryApiKey : "714814147251835",
+  cloudinaryApiSecret : "TtqMOLwi2GqyhU-uszWCZPvu6IY",
+  paypalClientId : "AbzJtJweNV-tYhUJGbtsGniirssvdLPCxbF-FTwAX_cbW-I9QqZ3JmPX-bzK202Dnt86qfTekmUG57R_",
+  geocodingBaseUrl : "https://eu1.locationiq.com/v1/search?key=",
+  geocodingToken : "pk.2cd804ffd67b9615096d4c3069990a53"
 };
